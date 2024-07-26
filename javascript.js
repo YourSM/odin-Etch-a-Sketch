95a458e46cd70e73c5121fade492bbb6fb827900
YourSM <156563558+YourSM@users.noreply.github.com>
2024-07-26 15:26:20 -0700

    Add funtionality behind creating a grid

---
+++ javascript.js
@@ -0,0 +1,26 @@
+const rowContainer = document.querySelector(".horizontalContainer");
+const masterContainer = document.querySelector(".masterContainer");
+const gridContainer = document.querySelector(".gridContainer");
+
+function createGrid(size) {
+for (let i = 0; i < size; i++) {
+  const column = document.createElement("div");
+  gridContainer.appendChild(column);
+  column.style.border = "5px solid black";
+  for (let i = 0; i < size; i++) {
+    let row = document.createElement("div");
+    row.classList.add("row");
+    row.style.border = "2px solid black";
+    row.style.width = "20px";
+    row.style.height = "20px";
+    column.appendChild(row);
+    }
+    masterContainer.appendChild(gridContainer);
+  }
+}
+
+createGrid(16);
+// create 16x16 grid of square divs
+// for loop going from 16 down/up to 16?
+//    createelement "div"?
+// divs in container (flexbox vertical)horizontalContainer
