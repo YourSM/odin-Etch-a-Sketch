0016227e9f706b25471180acc80d2fc6e1a704e1
YourSM <156563558+YourSM@users.noreply.github.com>
2024-07-27 12:51:23 -0700

    Add logic to make a new grid of the same size

--- javascript.js
+++ javascript.js
@@ -1,18 +1,23 @@
-const rowContainer = document.querySelector(".horizontalContainer");
 const interface = document.querySelector(".interface");
 const gridContainer = document.querySelector(".gridContainer");
+const newGrid = document.querySelector("#newGrid")
 
 function createGrid(size) {
 for (let i = 0; i < size; i++) {
   const column = document.createElement("div");
+  column.classList.add("column");
   gridContainer.appendChild(column);
-  column.style.border = "5px solid black";
+  column.style.border = "2px solid black";
+  column.style.display = "flex";
+  column.style.flex = "1"
+
   for (let i = 0; i < size; i++) {
     let row = document.createElement("div");
     row.classList.add("row");
     row.style.border = "2px solid black";
-    row.style.width = "20px";
-    row.style.height = "20px";
+    row.style.minWidth = "10px";
+    row.style.minHeight = "10px";
+    row.style.flex = "1"
     column.appendChild(row);
     row.addEventListener("mouseover", () => {
       row.style.backgroundColor = "black"
@@ -24,6 +29,12 @@ for (let i = 0; i < size; i++) {
 
 createGrid(16);
 
-
+newGrid.addEventListener("click", () => {
+  let columns = document.querySelectorAll(".column")
+  columns.forEach((item) => {
+    item.remove();
+  })
+  createGrid(prompt())
+})
 
 
