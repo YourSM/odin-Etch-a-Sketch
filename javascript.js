94e05b53664b91d4c81e9caea6b37f3b64e57aa2
YourSM <156563558+YourSM@users.noreply.github.com>
2024-07-27 10:45:27 -0700

    Add drawing functionality to grid

--- javascript.js
+++ javascript.js
@@ -14,13 +14,16 @@ for (let i = 0; i < size; i++) {
     row.style.width = "20px";
     row.style.height = "20px";
     column.appendChild(row);
+    row.addEventListener("mouseover", () => {
+      row.style.backgroundColor = "black"
+  })
     }
     interface.appendChild(gridContainer);
   }
 }
 
 createGrid(16);
-// create 16x16 grid of square divs
-// for loop going from 16 down/up to 16?
-//    createelement "div"?
-// divs in container (flexbox vertical)horizontalContainer
+
+
+
+
