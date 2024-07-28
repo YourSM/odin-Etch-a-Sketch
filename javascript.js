72aba4666cb1f9e4d681f58e5dfaf0ceb2e4db7d
YourSM <156563558+YourSM@users.noreply.github.com>
2024-07-28 15:45:39 -0700

    Add logic behind blackout mode

--- javascript.js
+++ javascript.js
@@ -2,7 +2,7 @@ const interface = document.querySelector(".interface");
 const gridContainer = document.querySelector(".gridContainer");
 const newGrid = document.querySelector("#newGrid");
 const RGBMode = document.querySelector("#RGBMode");
-
+const blackoutMode = document.querySelector("#blackoutMode");
 
 function createGrid(size) {
 for (let i = 0; i < size; i++) {
@@ -12,7 +12,6 @@ for (let i = 0; i < size; i++) {
   column.style.border = "2px solid black";
   column.style.display = "flex";
   column.style.flex = "1"
-
   for (let i = 0; i < size; i++) {
     let row = document.createElement("div");
     row.classList.add("row");
@@ -24,25 +23,12 @@ for (let i = 0; i < size; i++) {
     row.addEventListener("mouseover", () => {
     row.style.backgroundColor = "black"
     
-    // Add function to make random colors. Then attach function here.
-    // It should add with same thing (the backgroundcolor thing) but with
-    // the rgb values replacing it. Make sure to do the placeholder
-    // and fill it with (row) so it isn't yelling about how it doesn't
-    // exist. Can be something like (tile) or something as a placeholder.
-    // Here as well will be the similar opacity one. I suppose it will
-    // Be basically everytime it hovers over it the function will
-    // play and reduce by 10 (maybe add a value and subtract from that
-    // value in the function and then plug that value as the value of
-    // the opacity. Like opacity = value - 1 or something). Thus that value
-    // may need to be in the global scope or something. Figure it out.
-    // RGB function should use a math.floor and *100 (and maybe rounding)
-    // thing to make the value random. Then you can do this for three values,
-    // R, G, and B, and plug those values into the backgroundColor like (R,G,B).
   })
     }
     interface.appendChild(gridContainer);
   }
-  addRGB()
+  addRGB();
+  addBlackout();
 }
 
 newGrid.addEventListener("click", () => {
@@ -71,6 +57,8 @@ function getRGBValues () {
   return rgb = "rgb(" + r + "," + g + "," + b + ")"
 }
 
+
+
 function addRGB () {
   const rows = document.querySelectorAll(".row")
   RGBMode.addEventListener("click", () => {  
@@ -79,5 +67,26 @@ function addRGB () {
       row.style.backgroundColor = getRGBValues()
       })
     }) 
+  })
+}
+
+function lowerOpacity(target) {
+  let currentOpacity = target.style.opacity
+  if (+currentOpacity === 1) {
+    return target.style.opacity = 1;
+  } else {
+  return target.style.opacity = (currentOpacity - -0.1) ;
+}
+}
+
+function addBlackout () {
+  const rows = document.querySelectorAll(".row");
+  blackoutMode.addEventListener("click", () => {
+    rows.forEach((row) => {
+      row.addEventListener("mouseenter", (event) => {
+      let target = event.target;
+          target.style.opacity = lowerOpacity(target);
+    })
+    })
   })
 }
